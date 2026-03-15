70a9a6c569a035c011b653604eb7c9ff9a57908a
Moshe Immerman <moshe@flanksource.com>
2026-03-15 17:30:40 +0200

    test: add Ginkgo e2e tests for Helm chart with sandbox
    Go-based e2e tests using commons-test/helm that build the Docker image,
    install the chart on Kind, and verify healthz, ConfigMap, render endpoint,
    and sandbox disable/enable. Updates CI workflow to run Go e2e tests.

---
+++ chart/test/helm_test.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Facet", Ordered, FlakeAttempts(3), func() {
+
+	Context("Chart Installation", func() {
+		It("should be deployed", func() {
+			status, err := chart.GetStatus()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status.Info.Status).To(Equal("deployed"))
+		})
+	})
+
+	Context("ConfigMap", func() {
+		It("should create srt-settings ConfigMap", func() {
+			cm := chart.GetConfigMap("facet-test-srt-settings")
+			val, err := cm.Get("srt-settings.json")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(val).NotTo(BeEmpty())
+
+			var settings map[string]any
+			Expect(json.Unmarshal([]byte(val), &settings)).NotTo(HaveOccurred())
+			Expect(settings).To(HaveKey("enableWeakerNestedSandbox"))
+			Expect(settings).To(HaveKey("network"))
+			Expect(settings).To(HaveKey("filesystem"))
+		})
+	})
+
+	Context("Health", func() {
+		It("should respond on /healthz", func() {
+			resp, err := http.Get(fmt.Sprintf("http://localhost:%d/healthz", *localPort))
+			Expect(err).NotTo(HaveOccurred())
+			defer resp.Body.Close()
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+			body, err := io.ReadAll(resp.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(body)).To(ContainSubstring("ok"))
+		})
+	})
+
+	Context("Render", func() {
+		It("should accept render requests", func() {
+			payload := `{"template":"SimpleReport","format":"html","data":{"title":"Test Report"}}`
+			client := &http.Client{Timeout: 120 * time.Second}
+			resp, err := client.Post(
+				fmt.Sprintf("http://localhost:%d/render", *localPort),
+				"application/json",
+				strings.NewReader(payload),
+			)
+			Expect(err).NotTo(HaveOccurred())
+			defer resp.Body.Close()
+
+			body, _ := io.ReadAll(resp.Body)
+			GinkgoWriter.Printf("Render response (%d): %s\n", resp.StatusCode, string(body))
+			Expect(resp.StatusCode).To(BeNumerically("<", 500), "Server error: %s", string(body))
+		})
+	})
+
+	Context("Sandbox Disabled", func() {
+		It("should work without sandbox", func() {
+			By("Upgrading chart with sandbox disabled")
+			chart.SetValue("sandbox.enabled", false)
+			Expect(chart.Upgrade()).NotTo(HaveOccurred())
+
+			By("Verifying ConfigMap is gone")
+			cm := chart.GetConfigMap("facet-test-srt-settings")
+			_, err := cm.Get("srt-settings.json")
+			Expect(err).To(HaveOccurred())
+
+			By("Verifying healthz still works")
+			pod := chart.GetPod("app.kubernetes.io/name=facet")
+			pod.WaitReady()
+			Expect(pod.Error()).NotTo(HaveOccurred())
+
+			newPort, newStop := pod.ForwardPort(3000)
+			Expect(newPort).NotTo(BeNil())
+			defer newStop()
+
+			resp, err := http.Get(fmt.Sprintf("http://localhost:%d/healthz", *newPort))
+			Expect(err).NotTo(HaveOccurred())
+			defer resp.Body.Close()
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+			By("Re-enabling sandbox for cleanup")
+			chart.SetValue("sandbox.enabled", true)
+			Expect(chart.Upgrade()).NotTo(HaveOccurred())
+		})
+	})
+})
---
+++ chart/test/suite_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	flanksourceCtx "github.com/flanksource/commons-db/context"
+	"github.com/flanksource/commons-test/helm"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var (
+	kubeconfig string
+	namespace  = "facet-test"
+	chart      *helm.HelmChart
+	localPort  *int
+	stopFwd    func()
+	imageTag   = "test"
+)
+
+func findParentDir(dir string) string {
+	currentDir, _ := os.Getwd()
+	for {
+		if _, err := os.Stat(filepath.Join(currentDir, dir)); err == nil {
+			return filepath.Join(currentDir, dir)
+		}
+		if _, err := os.Stat(filepath.Join(currentDir, ".git")); err == nil {
+			return currentDir
+		}
+		currentDir = filepath.Dir(currentDir)
+	}
+}
+
+func findRepoRoot() string {
+	currentDir, _ := os.Getwd()
+	for {
+		if _, err := os.Stat(filepath.Join(currentDir, "Dockerfile")); err == nil {
+			return currentDir
+		}
+		parent := filepath.Dir(currentDir)
+		if parent == currentDir {
+			return ""
+		}
+		currentDir = parent
+	}
+}
+
+func TestHelm(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Facet Helm Chart Suite")
+}
+
+var _ = BeforeSuite(func() {
+	kubeconfig = os.Getenv("KUBECONFIG")
+	if kubeconfig == "" {
+		kubeconfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	}
+
+	if stat, err := os.Stat(kubeconfig); err != nil || stat.IsDir() {
+		path, _ := filepath.Abs(kubeconfig)
+		Skip(fmt.Sprintf("KUBECONFIG %s is not valid, skipping helm tests", path))
+	}
+
+	if ns := os.Getenv("TEST_NAMESPACE"); ns != "" {
+		namespace = ns
+	}
+
+	if tag := os.Getenv("IMAGE_TAG"); tag != "" {
+		imageTag = tag
+	}
+
+	repoRoot := findRepoRoot()
+	Expect(repoRoot).NotTo(BeEmpty(), "Could not find repo root with Dockerfile")
+
+	By("Building Docker image")
+	imageName := fmt.Sprintf("ghcr.io/flanksource/facet:%s", imageTag)
+	cmd := exec.Command("docker", "build", "-t", imageName, repoRoot)
+	cmd.Stdout = GinkgoWriter
+	cmd.Stderr = GinkgoWriter
+	Expect(cmd.Run()).NotTo(HaveOccurred(), "Docker build failed")
+
+	chartPath := findParentDir("chart")
+	GinkgoWriter.Printf("kubeconfig=%s namespace=%s chart=%s image=%s\n", kubeconfig, namespace, chartPath, imageName)
+
+	ctx := flanksourceCtx.New().WithNamespace(namespace)
+	chart = helm.NewHelmChart(ctx, chartPath)
+
+	By("Installing facet chart")
+	chart.Release("facet-test").
+		Namespace(namespace).
+		Values(map[string]any{
+			"image": map[string]any{
+				"tag":        imageTag,
+				"pullPolicy": "Never",
+			},
+			"sandbox": map[string]any{
+				"enabled": true,
+			},
+		}).
+		WaitFor(3 * time.Minute)
+
+	if err := chart.Install(); err != nil {
+		Expect(chart.Upgrade()).NotTo(HaveOccurred())
+	}
+
+	By("Port-forwarding to facet pod")
+	pod := chart.GetPod("app.kubernetes.io/name=facet")
+	pod.WaitReady()
+	Expect(pod.Error()).NotTo(HaveOccurred())
+
+	localPort, stopFwd = pod.ForwardPort(3000)
+	Expect(localPort).NotTo(BeNil())
+})
+
+var _ = AfterSuite(func() {
+	if stopFwd != nil {
+		stopFwd()
+	}
+	if chart != nil {
+		chart.Delete()
+	}
+})
